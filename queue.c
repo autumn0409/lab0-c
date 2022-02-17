fa0c7728ff504f2568acedfa806d8564f51c59cc
autumn0409 <su850409@gmail.com>
2022-02-17 12:29:34 +0800

    Implement q_new

--- queue.c
+++ queue.c
@@ -17,7 +17,9 @@
  */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *new_head = malloc(sizeof(struct list_head));
+    INIT_LIST_HEAD(new_head);
+    return new_head;
 }
 
 /* Free all storage used by queue */
